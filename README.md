b9faf1841ba2c72cf7bd7f77d79da9f4b3795384
Duke-of-Karamel <lukyn.wagner@seznam.cz>
2025-03-20 23:41:47 +0100

    fix: implemented hotfix backport

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# THIS IS A FORK FOR SEVTECH FIX
+
 # Refined Storage [![Build Status](https://jenkins.raoulvdberge.com/buildStatus/icon?job=refinedstorage)](https://jenkins.raoulvdberge.com/job/refinedstorage/) [![CurseForge](http://cf.way2muchnoise.eu/full_243076_downloads.svg)](http://minecraft.curseforge.com/projects/refined-storage)
 
 **Refined Storage is a mass storage mod for Minecraft that offers the player a network-based storage system, allowing them to store items and fluids on a massively expandable device network.**
@@ -5,3 +7,11 @@
 Items and fluids can be stored in one of the many storage capabilities that the mod offers. Any storage devices connected to the same network are accessible to the player through one simple Grid interface, allowing the player to access the inventories of many devices through a single unified GUI.
 
 This mod not only adds storage solutions, but also devices that can be used to manipulate items and blocks in the world and from within the system, such as importers, exporters, constructors, destructors, and more! There are also devices in this mod that allow the player to setup auto-crafting, allowing the crafting of complex recipes in a few simple clicks.
+
+## THE FIX
+This fork backports original hotfix for game crashing when clicking recipes into grid.
+Original hotfix: https://github.com/Darkere/refinedstorage/commit/68411ae369df0ba476c427b1c53a1604042795ae
+Original issue: https://github.com/refinedmods/refinedstorage/issues/1790
+
+Modern refined storage splits the packets, while this hotfix simply refuses to send too large packets.
+(clicking in recipes with too many block alternatives (ie. wood types) will not work)
--- src/main/java/com/raoulvdberge/refinedstorage/network/MessageGridTransfer.java
+++ src/main/java/com/raoulvdberge/refinedstorage/network/MessageGridTransfer.java
@@ -4,7 +4,7 @@ import com.raoulvdberge.refinedstorage.api.network.grid.GridType;
 import com.raoulvdberge.refinedstorage.api.network.grid.IGrid;
 import com.raoulvdberge.refinedstorage.container.ContainerGrid;
 import com.raoulvdberge.refinedstorage.util.StackUtils;
-import io.netty.buffer.ByteBuf;
+import io.netty.buffer.*;
 import mezz.jei.api.gui.IGuiIngredient;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.Slot;
@@ -20,6 +20,7 @@ public class MessageGridTransfer extends MessageHandlerPlayerToServer<MessageGri
     private List<Slot> slots;
 
     private ItemStack[][] recipe = new ItemStack[9][];
+    private io.netty.buffer.ByteBuf buffer = Unpooled.buffer();
 
     public MessageGridTransfer() {
     }
@@ -27,6 +28,37 @@ public class MessageGridTransfer extends MessageHandlerPlayerToServer<MessageGri
     public MessageGridTransfer(Map<Integer, ? extends IGuiIngredient<ItemStack>> inputs, List<Slot> slots) {
         this.inputs = inputs;
         this.slots = slots;
+        createBuffer();
+    }
+
+    private void createBuffer() {
+        buffer.writeInt(slots.size());
+
+        // traverse crafting grid slots
+        for (Slot slot : slots) {
+            IGuiIngredient<ItemStack> ingredient = inputs.get(slot.getSlotIndex() + 1);
+
+            List<ItemStack> ingredients = new ArrayList<>();
+
+            // if not empty ingredient add all possible substitutions (wood types, etc)
+            if (ingredient != null) {
+                for (ItemStack possibleStack : ingredient.getAllIngredients()) {
+                    if (possibleStack != null) {
+                        ingredients.add(possibleStack); // This breaks limit when too many wood types are present for example
+                    }
+                }
+            }
+
+            buffer.writeInt(ingredients.size());
+
+            for (ItemStack possibleStack : ingredients) {
+                StackUtils.writeItemStack(buffer, possibleStack);
+            }
+        }
+
+        if (buffer.array().length > 32767){
+            throw new IllegalArgumentException("Payload may not be larger than 32767 bytes");
+        }
     }
 
     @Override
@@ -46,27 +78,7 @@ public class MessageGridTransfer extends MessageHandlerPlayerToServer<MessageGri
 
     @Override
     public void toBytes(ByteBuf buf) {
-        buf.writeInt(slots.size());
-
-        for (Slot slot : slots) {
-            IGuiIngredient<ItemStack> ingredient = inputs.get(slot.getSlotIndex() + 1);
-
-            List<ItemStack> ingredients = new ArrayList<>();
-
-            if (ingredient != null) {
-                for (ItemStack possibleStack : ingredient.getAllIngredients()) {
-                    if (possibleStack != null) {
-                        ingredients.add(possibleStack);
-                    }
-                }
-            }
-
-            buf.writeInt(ingredients.size());
-
-            for (ItemStack possibleStack : ingredients) {
-                StackUtils.writeItemStack(buf, possibleStack);
-            }
-        }
+        buf.writeBytes(buffer);
     }
 
     @Override
